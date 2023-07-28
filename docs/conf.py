66e0559685888e234c44abfa53f9d7f134b797e1
Saurabh Pradhan(on Thinkstation) <pradhanphy@gmail.com>
2023-07-28 10:19:52 +0200

    change readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,17 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"  # "sphinx_rtd_theme"
+# sidebar
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+        "donate.html",
+    ]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
