324eb3d703ef2482f366e47de703be35902e62b1
Saurabh Pradhan <1505628+srbhp@users.noreply.github.com>
2023-10-08 18:09:43 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,8 @@
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("."))
+# -- Project Information----------------------------------
 project = "nrgplusplus"
 copyright = "2017-2022, Saurabh Pradhan"
 author = "Saurabh Pradhan"
