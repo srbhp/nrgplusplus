c7be3905a6fc198b94137dcfd699fdf31133559a
Saurabh Pradhan <1505628+srbhp@users.noreply.github.com>
2023-10-08 17:55:29 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 # [[[ end theme marker ]]]
 
 rst_epilog = ".. |theme| replace:: ``{0}``".format(html_theme)
